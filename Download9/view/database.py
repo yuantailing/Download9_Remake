750a8a57362ae5d4598b1c9837ae702d41b0751f
tgopknight <tgopknight@163.com>
2019-03-13 03:17:59 +0800

    Fixed problem 'out of sync' when multithreading

--- Download9/view/database.py
+++ Download9/view/database.py
@@ -8,6 +8,9 @@ CHARSET = "utf8"
 
 db = MySQLdb.connect(host=HOST, user=USER, passwd=PASSWD, db=DB, charset=CHARSET)
 
+import threading
+lock = threading.Lock()
+
 def id(Table, Column):
     if (Table == "account"):
         if (Column == "username"):
@@ -34,27 +37,30 @@ def id(Table, Column):
     else:
         assert 0
 
+def run(Str, NeedResult = True):
+    lock.acquire()
+    cursor = db.cursor()
+    try:
+        cursor.execute(Str)
+        db.commit()
+        if (NeedResult):
+            ret = cursor.fetchall()
+    except:
+        db.rollback()
+    cursor.close()
+    lock.release()
+    if (NeedResult):
+        return ret
+
 def get_all(Table, Rules):
     if (len(Rules) == 0):
-        cursor = db.cursor()
-        '''print("SELECT * FROM " + Table + ";")'''
-        cursor.execute("SELECT * FROM " + Table + ";")
-        db.commit()
-        ret = cursor.fetchall()
-        cursor.close()
-        return ret
+        return run("SELECT * FROM " + Table + ";")
     s = ""
     for (i, j) in Rules.items():
         if isinstance(j, str):
             j = "\"" + j + "\""
         s = s + i + "=" + j + " AND "
-    cursor = db.cursor()
-    '''print("SELECT * FROM " + Table + " WHERE " + s[:-5] + ";")'''
-    cursor.execute("SELECT * FROM " + Table + " WHERE " + s[:-5] + ";")
-    db.commit()
-    ret = cursor.fetchall()
-    cursor.close()
-    return ret
+    return run("SELECT * FROM " + Table + " WHERE " + s[:-5] + ";")
 
 def find_single(Table, Rules):
     res = get_all(Table, Rules)
@@ -77,14 +83,7 @@ def update(Table, Object, NewArgv):
         if isinstance(j, str):
             j = "\"" + j + "\""
         NewArgvs = NewArgvs + i + "=" + str(j) + ","
-    cursor = db.cursor()
-    '''print("UPDATE " + Table + " SET " + NewArgvs[:-1] + " WHERE " + Objects[:-5] + ";")'''
-    try:
-        cursor.execute("UPDATE " + Table + " SET " + NewArgvs[:-1] + " WHERE " + Objects[:-5] + ";")
-        db.commit()
-    except:
-        db.rollback()
-    cursor.close()
+    run("UPDATE " + Table + " SET " + NewArgvs[:-1] + " WHERE " + Objects[:-5] + ";", False)
 
 def delete(Table, Rules):
     if (len(Rules) == 0): return
@@ -93,14 +92,7 @@ def delete(Table, Rules):
         if isinstance(j, str):
             j = "\"" + j + "\""
         s = s + i + "=" + str(j) + " AND "
-    cursor = db.cursor()
-    '''print("DELETE FROM " + Table + " WHERE " + s[:-5] + ";")'''
-    try:
-        cursor.execute("DELETE FROM " + Table + " WHERE " + s[:-5] + ";")
-        db.commit()
-    except:
-        db.rollback()
-    cursor.close()
+    run("DELETE FROM " + Table + " WHERE " + s[:-5] + ";", False)
 
 def insert(Table, Object):
     if (len(Object) == 0): return
@@ -113,11 +105,4 @@ def insert(Table, Object):
         Values = Values + str(j) + ","
     Fields = Fields[:-1] + ")"
     Values = Values[:-1] + ")"
-    cursor = db.cursor()
-    '''print("INSERT INTO " + Table + " " + Fields + " VALUES " + Values + ";")'''
-    try:
-        cursor.execute("INSERT INTO " + Table + " " + Fields + " VALUES " + Values + ";")
-        db.commit()
-    except:
-        db.rollback()
-    cursor.close()
+    run("INSERT INTO " + Table + " " + Fields + " VALUES " + Values + ";", False)
